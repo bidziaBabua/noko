729e62ee4fc7e902850479dca2552149d4e983ba
gnems <g_nemsveridze@cu.edu.ge>
2018-09-14 00:19:13 +0400

    landing page

---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>ნორიკო პრეზიდენტად</title>
+<link rel="icon" href="images/favicon.jpg" type="image/jpg" sizes="16x16">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ნორიკო თავბერიძის პერსონალური გვერდი">
+<meta name="keywords" content="ნორიკო თავბერიძე, არჩევნები">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+<link rel="stylesheet" href="css/animate.css">
+<!-- Resource style -->
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/ionicons.min.css">
+<!-- Resource style -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/newStyle.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+<div class="wrapper">
+  <div class="container">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container"> 
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          <a class="navbar-brand page-scroll" href="#main"><img src="images/sample_logo.png" width="150" height="40" alt="iLand" /></a> </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a class="page-scroll ns-navbar" href="#main">მთავარი</a></li>
+            <li><a class="page-scroll ns-navbar" href="#about_us">ვინ ვართ</a></li>
+            <li><a class="page-scroll ns-navbar" href="#we_think">რას ვფიქრობთ</a></li>
+            <li><a class="page-scroll ns-navbar" href="#we_change">რას შევცვლით</a></li>
+            <li><a class="page-scroll ns-navbar" href="#program">ჩვენი პროგრამა</a></li>
+            <li><a class="page-scroll ns-navbar" href="#contact">კონტაქტი</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid -->
+  
+  <div class="main app form" id="main"><!-- Main Section-->
+    <div class="hero-section">
+      <div class="container nopadding">
+        <div class="col-md-12">
+          <div class="hero-content text-center">
+            <h1 class="wow fadeInUp" data-wow-delay="0.1s">ჩვენ არ გაჭმევთ ასფალტს</h1>
+            <p class="wow fadeInUp" data-wow-delay="0.2s">საქართველოს პრეზიდენტობის კანდიდატი ნორიკო თავბერიძე. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Who we are Section -->
+    <div class="review-section" id="about_us">
+      <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="reviews owl-carousel owl-theme">
+            <div class="review-single"><img class="img-circle" src="images/customer1.png" alt="Client Testimonoal" />
+              <div class="review-text wow fadeInUp" data-wow-delay="0.2s">
+                <p>სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი<br /> 
+                სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი </p>
+                <h3>- სახელი გვარი</h3>
+                <h3>სპასპეტი არაბეთში</h3>
+              </div>
+            </div>
+            <div class="review-single"><img class="img-circle" src="images/customer2.png" alt="Client Testimonoal" />
+              <div class="review-text">
+              <p>სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი<br /> 
+                სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი </p>
+                <h3>- სახელი გვარი</h3>
+                <h3>სტაიანშიკი სამგორის მეტროსთან</h3>
+              </div>
+            </div>
+            <div class="review-single"><img class="img-circle" src="images/customer3.png" alt="Client Testimonoal" />
+              <div class="review-text">
+              <p>სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი<br /> 
+                სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი </p>
+                <h3>- სახელი გვარი</h3>
+                <h3>ავტობუსის კონტროლიორი</h3>
+              </div>
+            </div>
+            <div class="review-single"><img class="img-circle" src="images/customer4.png" alt="Client Testimonoal" />
+              <div class="review-text">
+              <p>სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი<br /> 
+                სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი </p>
+                <h3>- სახელი გვარი</h3>
+                <h3>მესამე დასის ანგელოზი ქერუბიმის კარზე</h3>
+              </div>
+            </div>
+            <div class="review-single"><img class="img-circle" src="images/customer5.png" alt="Client Testimonoal" />
+              <div class="review-text">
+              <p>სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი<br /> 
+                სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი სფიჩი </p>
+                <h3>- სახელი გვარი</h3>
+                <h3>უბრალოდ კარგი ადამიანი</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- What we think Section -->
+    <div class="app-features text-center" id="we_think">
+      <div class="container">
+        <h1 class="wow fadeInDown" data-wow-delay="0.1s">ჩვენ ვთვლით, რომ</h1>
+        <div class="col-md-4 features-left text-right">
+          <div class="col-md-12 wow fadeInDown" data-wow-delay="0.2s">
+            <div class="icon"> <i class="ion-ios-list-outline"></i> </div>
+            <div class="feature-single">
+              <p>ეს უნდა დასრულდეს, იმიტო რო რაღაცა ხო გვაქ შესაცვლელი და რა ვქნათ.</p>
+            </div>
+          </div>
+          <div class="col-md-12 wow fadeInDown" data-wow-delay="0.3s">
+            <div class="icon"> <i class="ion-ios-speedometer-outline"></i> </div>
+            <div class="feature-single">
+              <p>უნდა დაიწყოს ამისა და ამის კონტროლი და აღარ იყოს იმისა და იმის მსგავსად.</p>
+            </div>
+          </div>
+          <div class="col-md-12 wow fadeInDown" data-wow-delay="0.4s">
+            <div class="icon"> <i class="ion-ios-pulse"></i> </div>
+            <div class="feature-single">
+              <p>ეს უნდა დასრულდეს, იმიტო რო რაღაცა ხო გვაქ შესაცვლელი და რა ვქნათ</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-delay="0.5s"> <img class="img-responsive" src="images/success.jpg" alt="App" /> </div>
+        <div class="col-md-4 features-left text-left">
+          <div class="col-md-12 wow fadeInDown" data-wow-delay="0.6s">
+            <div class="icon"> <i class="ion-ios-americanfootball-outline"></i> </div>
+            <div class="feature-single">
+            <p>უნდა დაიწყოს ამისა და ამის კონტროლი და აღარ იყოს იმისა და იმის მსგავსად.</p>
+            </div>
+          </div>
+          <div class="col-md-12 wow fadeInDown" data-wow-delay="0.7s">
+            <div class="icon"> <i class="ion-ios-heart-outline"></i> </div>
+            <div class="feature-single">
+              <p>ეს უნდა დასრულდეს, იმიტო რო რაღაცა ხო გვაქ შესაცვლელი და რა ვქნათ</p>
+            </div>
+          </div>
+          <div class="col-md-12 wow fadeInDown" data-wow-delay="0.8s">
+            <div class="icon"> <i class="ion-ios-analytics-outline"></i> </div>
+            <div class="feature-single">
+            <p>უნდა დაიწყოს ამისა და ამის კონტროლი და აღარ იყოს იმისა და იმის მსგავსად.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+     <!-- Promises Section -->
+     <div class="pitch text-center" id="we_change">
+      <div class="container">
+        <div class="pitch-intro">
+          <h1 class="wow fadeInDown" data-wow-delay="0.2s">ჩვენ შევცვლით</h1>
+        </div>
+        <div class="col-md-12">
+          <div class="col-md-4 wow fadeInDown" data-wow-delay="0.2s">
+            <div class="pitch-icon"> <i class="ion-ios-checkmark-outline"></i> </div>
+            <div class="pitch-content">
+              <h1>მაკდონალდსის მენიუს</h1>
+              <p> ჰამბურგერში იქნება მეტი მდნარი ყველი, რაც გაზრდის იმ კლიენტების სტუმრიანობას, რომლებიც ტოსტს ვუკვეთავთ ახლა. </p>
+            </div>
+          </div>
+          <div class="col-md-4 wow fadeInDown" data-wow-delay="0.2s">
+            <div class="pitch-icon"> <i class="ion-ios-mic-outline"></i> </div>
+            <div class="pitch-content">
+              <h1>მარშუტკის რადიოს</h1>
+              <p>სახელმწიფოს ხარჯზე გავაკეთებთ მარშუტკის რადიოს, სადაც მხოლოდ ბრიტნი სპირსისა და კრისწინა აგილერას სიმღერები იქნება. </p>
+            </div>
+          </div>
+          <div class="col-md-4 wow fadeInDown" data-wow-delay="0.2s">
+            <div class="pitch-icon"> <i class="ion-ios-folder-outline"></i> </div>
+            <div class="pitch-content">
+              <h1>სკეიტ-პარკს</h1>
+              <p>ან სკეიტ-პარკს გავაუქმებთ დედაენის ბაღში ან საერთოდ დედაენის ბაღს; გავზრდით იუსტიციის სახლს და დავასაქმებთ მეტ სტაიანშიკს. </p>
+            </div>
+          </div>
+          <div class="col-md-4 wow fadeInDown" data-wow-delay="0.2s">
+            <div class="pitch-icon"> <i class="ion-ios-folder-outline"></i> </div>
+            <div class="pitch-content">
+              <h1>საზოგადოებრივ მაუწყებელს</h1>
+              <p> მომდევნო სეზონიდანაც გავუშვებთ ჩემპიონთა ლიგას, თორე რა ვქნა, მართლა არ ვიცი, საით წევიდე, აჩვენებს სილქ უნივერსალი კი?. </p>
+            </div>
+          </div>
+          <div class="col-md-4 wow fadeInDown" data-wow-delay="0.2s">
+            <div class="pitch-icon"> <i class="ion-ios-folder-outline"></i> </div>
+            <div class="pitch-content">
+              <h1>უნივერსიტეტებს</h1>
+              <p> ყველა ნახევარ-უნივერსიტეტი, სადაც ბავშვები მარტო აბაებენ და მერე, მგოჭამა ჭირი, მთელი 4 წელი აჭარაში მოგებული ფულით შაურმის ჭამაში გადის, უნდა გაუქმდეს.. </p>
+            </div>
+          </div>
+          <div class="col-md-4 wow fadeInDown" data-wow-delay="0.2s">
+            <div class="pitch-icon"> <i class="ion-ios-folder-outline"></i> </div>
+            <div class="pitch-content">
+              <h1>შაურმის ბიზნესს</h1>
+              <p> ავკრძალავთ გლდანის შაურმას და გლდანის შაურმის კუთვნილ ზავეძენიებს გადავცემთ ფრაიბურგის შაურმას, რათა უფრო ჯანსაღი და ლურჯკომბოსტოიანი შაურმა ჭამოს ხალხმა.. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Program Section -->
+    <div id="program" class="pricing-section text-center">
+      <div class="container">
+        <div class="col-md-12 col-sm-12 nopadding">
+          <div class="pricing-intro">
+            <h1 class="wow fadeInUp" data-wow-delay="0s">ჩვენი პროგრამა</h1>
+          </div>
+          <div class="col-sm-6">
+            <div class="table-left wow fadeInUp" data-wow-delay="0.4s">
+              <div class="pricing-details">
+                <span>ჯანდაცვა</span>
+                <ul class="a">
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                </ul>
+                <button class="btn btn-primary btn-action btn-fill">ვრცლად</button>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="table-right wow fadeInUp" data-wow-delay="0.6s">
+              <div class="pricing-details">
+              <span>ეკონომიკა</span>
+                <ul class="a">
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                </ul>
+                <button class="btn btn-primary btn-action btn-fill">ვრცლად</button>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="table-right wow fadeInUp" data-wow-delay="0.6s">
+              <div class="pricing-details">
+              <span>განათლება</span>
+                <ul class="a">
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                </ul>
+                <button class="btn btn-primary btn-action btn-fill">ვრცლად</button>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="table-right wow fadeInUp" data-wow-delay="0.6s">
+              <div class="pricing-details">
+              <span style='font-size: 42px; margin-bottom: 25px;'>საგარეო პოლიტიკა</span>
+                <ul class="a">
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                  <li>პროგრამა პროგრამა პროგრამა პროგრამა პროგრამა</li>
+                </ul>
+                <button class="btn btn-primary btn-action btn-fill">ვრცლად</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Contact Form -->
+    <div class="cta-sub no-color" id='contact'>
+      <div class="container">
+        <div class="cta-inner">
+          <h1 class="wow fadeInUp" data-wow-delay="0s">აქ ნოკოს ფბ-ს და ნომრებს ვწერთ</h1>
+          <p class="wow fadeInUp" data-wow-delay="0.2s"></p>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Footer Section -->
+    <div class="footer" id="contact">
+      <div class="container">
+        <div class="col-md-6 contact">
+        </div>
+        <div class="col-md-3 contact">
+          <p> პრობლემის აღმოჩენის შემთხვევაში დაგვიკავშირდით:</p>
+          <a href="mailto:g_nemsveridze@cu.edu.ge">g_nemsveridze@cu.edu.ge</a> </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Scroll To Top --> 
+  
+  <a id="back-top" class="back-to-top page-scroll" href="#main"> <i class="ion-ios-arrow-thin-up"></i> </a> 
+  
+  <!-- Scroll To Top Ends--> 
+  
+</div>
+<!-- Main Section -->
+</div>
+<!-- Wrapper--> 
+
+<!-- Jquery and Js Plugins --> 
+<script type="text/javascript" src="js/jquery-2.1.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="js/plugins.js"></script> 
+<script type="text/javascript" src="js/menu.js"></script> 
+<script type="text/javascript" src="js/custom.js"></script> 
+<script src="js/jquery.subscribe.js"></script>
+</body>
+</html>
